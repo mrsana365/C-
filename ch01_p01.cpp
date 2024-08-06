0a3c9f487c759a982b251a094108deecb7c44ee7
mrsana365 <mrsana365@gmail.com>
2024-08-06 15:40:05 +0530

    This is my C++ chapter 1 , whic having basics concempt code of it.

---
+++ ch01_p01.cpp
@@ -0,0 +1,22 @@
+
+//   First "Hello world" program.
+
+// #include<iostream>
+// int main (){
+//     std :: cout<< "Hello world";
+//     return 0;
+// }
+
+//------------------- OR --------------------------
+
+// #include<iostream>
+// using namespace std;
+// int main (){
+
+//     cout<<"Hello World";
+//     return 0;
+// }
+
+
+
+
---
+++ ch01_p02.cpp
@@ -0,0 +1,58 @@
+
+#include<iostream>
+using namespace std;
+int main (){
+    // cout<< " hello PW";
+    // cout<<endl;
+    // cout<< " hello PW 2";
+
+    // -----------  OR -------------------
+
+    // cout<< " hello PW";
+    // cout<<"\n";
+    // cout<< " hello PW 2";
+
+    //  -------- OR  ------------------------
+
+    // cout<< " hello PW"<<endl;
+    // cout<< " hello PW 2";
+
+    // cout<< " hello PW \n";
+    // cout<< " hello PW 2";
+
+
+
+    //  -------- OR  ------------------------
+
+    // cout<< " hello PW"<<endl<< " hello PW 2";
+    // cout<< "hello PW" <<"\n"<< "hello PW 2";
+
+    //  -------- OR  ------------------------
+
+    // cout<< "hello PW \n hello PW 2";
+
+    //  -------- OR  ------------------------
+
+    // cout<< " hello PW";
+    // cout<<endl;
+    // cout<<endl;
+    // cout<<" hello PW 2";
+
+    //  -------- OR  ------------------------
+
+    cout<< " hello PW \n\n hello PW 2";
+
+    //  -------- OR  ------------------------
+
+     cout<< "nn\nj\n nnn \n";
+     cout<<"nn/n/nnn/n";
+
+     
+
+
+
+    
+
+
+    return 0;
+}
